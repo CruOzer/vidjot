fd02ebe150b63679159087cb40887bca3bbf5be7
CruOzer <ChrisKost94@gmail.com>
2019-03-09 17:18:27 +0100

    process.port angepasst

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ const {
 app.use('/ideas', ensureAuthenticated, ideas);
 app.use('/users', users);
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server startet on ${port}`);
