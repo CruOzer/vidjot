9896799346ff4b04bf6a13c43231c6e64880d1dc
CruOzer <ChrisKost94@gmail.com>
2019-03-09 17:10:18 +0100

    DB Url angepasst

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'prod') {
     module.exports = {
-        mongoURI: "mongodb+srv://LC_rw_user:<NgILIxsd4bq5xvLt>@vidjot-prod-ypjkc.mongodb.net/test?retryWrites=true"
+        mongoURI: "mongodb+srv://LC_rw_user:NgILIxsd4bq5xvLt@vidjot-prod-ypjkc.mongodb.net/test?retryWrites=true"
     };
 } else {
     module.exports = {
