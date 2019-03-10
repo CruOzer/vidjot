2aad98dff3979a4ac6a03becebb850d34b07b705
CruOzer <ChrisKost94@gmail.com>
2019-03-10 08:49:13 +0100

    Github Commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: "mongodb+srv://LC_rw_user:NgILIxsd4bq5xvLt@vidjot-prod-ypjkc.mongodb.net/test?retryWrites=true"
+        mongoURI: "mongodb+srv://user:passwort@vidjot-prod-ypjkc.mongodb.net/test?retryWrites=true"
     };
 } else {
     module.exports = {
